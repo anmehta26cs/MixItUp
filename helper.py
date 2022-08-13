c2e085c2a94872298d10df545bc022e608418035
anmehta26 <anmehta26@gmail.com>
2022-08-13 11:32:20 -0500

    removed everything from main

--- helper.py
+++
@@ -1,8 +0,0 @@
-
-def get_playlist_tracks(sp, username,playlist_id):
-    results = sp.user_playlist_tracks(username,playlist_id)
-    tracks = results['items']
-    while results['next']:
-        results = sp.next(results)
-        tracks.extend(results['items'])
-    return tracks
